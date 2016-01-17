ae7629c5cb75d3aa481cae05959da604beaccc7e
Guillaume Turri <guillaume.turri@gmail.com>
2016-01-17 11:16:14 +0100

    Can parse times

--- src/main/java/fr/turri/jiso8601/Iso8601Deserializer.java
+++ src/main/java/fr/turri/jiso8601/Iso8601Deserializer.java
@@ -6,33 +6,94 @@ import java.util.TimeZone;
 
 public class Iso8601Deserializer {
 	public Calendar deserialize(String toParse){
-		Calendar result = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
-		result.set(Calendar.HOUR_OF_DAY, 0);
-		result.set(Calendar.MINUTE, 0);
-		result.set(Calendar.SECOND, 0);
-		result.set(Calendar.MILLISECOND, 0);
-		String basicFormatDate = toParse.replaceAll("-", "");
-		if ( basicFormatDate.length() == 2 ){
-			result.set(Integer.parseInt(basicFormatDate) * 100, 0, 1);
-			return result;
+		if ( toParse.indexOf('T') == -1 ){
+			return calendarWithDateOnly(toParse);
+		}
+		int indexOfT = toParse.indexOf('T');
+		Calendar result = calendarWithDateOnly(toParse.substring(0, indexOfT));
+		String basicFormatHour = toParse.substring(indexOfT+1).replace(":", "");
+
+		int indexOfZ = basicFormatHour.indexOf('Z');
+		if ( indexOfZ != -1 ){
+			parseHour(result, basicFormatHour.substring(0, indexOfZ));
+		} else {
+			int indexOfSign = getIndexOfSign(basicFormatHour);
+			if ( indexOfSign == -1 ){
+				parseHour(result, basicFormatHour);
+				result.setTimeZone(TimeZone.getDefault());
+			} else {
+				parseHour(result, basicFormatHour.substring(0, indexOfSign));
+				parseTimeZone(result, basicFormatHour.substring(indexOfSign));
+			}
+		}
+		return result;
+	}
+
+	private static int getIndexOfSign(String str){
+		int index = str.indexOf('+');
+		return index != -1 ? index : str.indexOf('-');
+	}
+
+	private void parseTimeZone(Calendar calendar, String tzStr){
+		calendar.setTimeZone(TimeZone.getTimeZone("GMT" + tzStr));
+	}
+
+	private void parseHour(Calendar calendar, String basicFormatHour){
+		basicFormatHour = basicFormatHour.replace(',', '.');
+		int indexOfDot = basicFormatHour.indexOf('.');
+		double fractionalPart = 0;
+		if ( indexOfDot != -1 ){
+			fractionalPart = Double.parseDouble("0" + basicFormatHour.substring(indexOfDot));
+			basicFormatHour = basicFormatHour.substring(0, indexOfDot);
 		}
 
-		if ( basicFormatDate.length() == 4){
-			result.set(Integer.parseInt(basicFormatDate), 0, 1);
-			return result;
+		if ( basicFormatHour.length() >= 2 ){
+			calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(basicFormatHour.substring(0, 2)));
 		}
 
-		int year = Integer.parseInt(basicFormatDate.substring(0, 4));
-		int month = Integer.parseInt(basicFormatDate.substring(4, 6)) - 1;
-		if ( basicFormatDate.length() == 6 ){
-			result.set(year, month, 1);
-			return result;
+		if ( basicFormatHour.length() > 2 ){
+			calendar.set(Calendar.MINUTE, Integer.parseInt(basicFormatHour.substring(2, 4)));
+		} else {
+			fractionalPart *= 60;
 		}
 
-		if ( basicFormatDate.length() == 8 ){
-			result.set(year, month, Integer.parseInt(basicFormatDate.substring(6)));
-			return result;
+		if ( basicFormatHour.length() > 4 ){
+			calendar.set(Calendar.SECOND, Integer.parseInt(basicFormatHour.substring(4, 6)));
+		} else {
+			fractionalPart *= 60;
 		}
-		throw new RuntimeException("Couldn't parse " + toParse);
+
+		calendar.set(Calendar.MILLISECOND, (int) (fractionalPart * 1000));
+	}
+
+	private static Calendar calendarWithDateOnly(String dateStr){
+			Calendar result = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
+			result.set(Calendar.HOUR_OF_DAY, 0);
+			result.set(Calendar.MINUTE, 0);
+			result.set(Calendar.SECOND, 0);
+			result.set(Calendar.MILLISECOND, 0);
+			String basicFormatDate = dateStr.replaceAll("-", "");
+			if ( basicFormatDate.length() == 2 ){
+				result.set(Integer.parseInt(basicFormatDate) * 100, 0, 1);
+				return result;
+			}
+
+			if ( basicFormatDate.length() == 4){
+				result.set(Integer.parseInt(basicFormatDate), 0, 1);
+				return result;
+			}
+
+			int year = Integer.parseInt(basicFormatDate.substring(0, 4));
+			int month = Integer.parseInt(basicFormatDate.substring(4, 6)) - 1;
+			if ( basicFormatDate.length() == 6 ){
+				result.set(year, month, 1);
+				return result;
+			}
+
+			if ( basicFormatDate.length() == 8 ){
+				result.set(year, month, Integer.parseInt(basicFormatDate.substring(6)));
+				return result;
+			}
+			throw new RuntimeException("Couldn't parse " + dateStr);
 	}
 }
---
+++ src/test/java/fr/turri/jiso8601/TestDeserializeTimes.java
@@ -0,0 +1,113 @@
+package fr.turri.jiso8601;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestDeserializeTimes {
+	private TimeZone _previousTZ;
+
+	@Before
+	public void setUp(){
+		_previousTZ = TimeZone.getDefault();
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+	@After
+	public void tearDown(){
+		TimeZone.setDefault(_previousTZ);
+	}
+
+	@Test
+	public void canParseBasicFormatHour(){
+		assertExpectedHour(12, 21, 36, "19850304T122136Z");
+	}
+
+	@Test
+	public void canParseExtendedFormatHour(){
+		assertExpectedHour(12, 21, 36, "1985-03-04T12:21:36Z");
+	}
+
+	@Test
+	public void canParseHourWithoutSeconds(){
+		assertExpectedHour(12, 21, 0, "1985-03-04T12:21Z");
+	}
+
+	@Test
+	public void canParseHourWithoutMinutes(){
+		assertExpectedHour(12, 0, 0, "1985-03-04T12Z");
+	}
+
+	@Test
+	public void canParseHourWithDecimalPartOfASecond(){
+		assertExpectedHour(12, 21, 36, 120, "1985-03-04T12:21:36.12Z");
+		assertExpectedHour(12, 21, 36, 120, "1985-03-04T12:21:36,12Z");
+	}
+
+	@Test
+	public void isntTooFarFromRealityWhenAVeryHighPrecisionIsProvided(){
+		assertExpectedHour(12, 21, 36, 123, "1985-03-04T12:21:36.123456Z");
+		assertExpectedHour(12, 21, 36, 123, "1985-03-04T12:21:36,123456Z");
+	}
+
+	@Test
+	public void canParseHourWithDecimalPartOfAMinute(){
+		assertExpectedHour(12, 21, 15, "1985-03-04T12:21.25Z");
+		assertExpectedHour(12, 21, 30, "1985-03-04T12:21.5Z");
+		assertExpectedHour(12, 21, 30, "1985-03-04T12:21,5Z");
+	}
+
+	@Test
+	public void canParseHourWithDecimalPartOfAnHour() {
+		assertExpectedHour(12, 15, 0, "1985-03-04T12.25");
+		assertExpectedHour(12, 0, 36, "1985-03-04T12.01");
+		assertExpectedHour(12, 0, 36, "1985-03-04T12,01");
+	}
+
+	@Test
+	public void useLocalTimeZoneWhenNoIndicationIsProvided(){
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		assertExpectedHour(12, 21, 36, "1985-03-04T12:21:36");
+
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Los_Angeles"));
+		assertExpectedHour(20, 21, 36, "1985-03-04T12:21:36");
+	}
+
+
+	@Test
+	public void canParseBasicFormatTimeOffset() {
+		assertExpectedHour(00, 51, 36, "19850304T122136+1130");
+	}
+
+	@Test
+	public void canParseExtendedFormatTimeOffset() {
+		assertExpectedHour(00, 51, 36, "1985-03-04T12:21:36+11:30");
+	}
+
+	@Test
+	public void canParseTimeOffsetWithoutMinute() {
+		assertExpectedHour(01, 21, 36, "1985-03-04T12:21:36+11");
+	}
+
+	@Test
+	public void canParseNegativeTimeOffset() {
+		assertExpectedHour(21, 41, 36, "1985-03-04T12:21:36-09:20");
+	}
+
+	private void assertExpectedHour(int hour, int minute, int second, String toParse){
+		assertExpectedHour(hour, minute, second, 0, toParse);
+	}
+
+	private void assertExpectedHour(int hour, int minute, int second, int millisecond, String toParse){
+		Calendar expected = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
+		expected.set(1985, Calendar.MARCH, 04, hour, minute, second);
+		expected.set(Calendar.MILLISECOND, millisecond);
+
+		assertEquals(0, expected.compareTo(new Iso8601Deserializer().deserialize(toParse)));
+	}
+}
