5c27c4b95d4162577bfe002e09e269f3e485ccca
Guillaume Turri <guillaume.turri@gmail.com>
2016-01-21 07:54:26 +0100

    Remove useless import

--- src/test/java/fr/turri/jiso8601/TestDeserializeTimes.java
+++ src/test/java/fr/turri/jiso8601/TestDeserializeTimes.java
@@ -4,8 +4,6 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
