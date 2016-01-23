19321a239f81ec46f0b759a73760e38d586de436
Guillaume Turri <guillaume.turri@gmail.com>
2016-01-23 11:32:54 +0100

    Add Android compatibility tests

---
+++ android-no-reg/app/src/androidTest/java/turri/fr/jiso8601_android_no_reg/ApplicationTest.java
@@ -0,0 +1,13 @@
+package turri.fr.jiso8601_android_no_reg;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
