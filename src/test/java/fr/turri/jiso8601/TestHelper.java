4ab83fdb72283638b9553a34816c724835ccf975
Jean-Baptiste Catté <jb.catte@criteo.com>
2018-09-18 13:33:13 +0000

    Improve test failure output & fix locale differences
    + Remove octal ints
    + Use package-private instead of protected

--- src/test/java/fr/turri/jiso8601/TestHelper.java
+++ src/test/java/fr/turri/jiso8601/TestHelper.java
@@ -1,13 +1,13 @@
 package fr.turri.jiso8601;
 
+import org.junit.After;
+import org.junit.Before;
+
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
-import org.junit.After;
-import org.junit.Before;
-
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestHelper {
 	private TimeZone _previousTZ;
@@ -22,11 +22,15 @@ public class TestHelper {
 	public void tearDown(){
 		TimeZone.setDefault(_previousTZ);
 	}
-	protected void assertExpectedDate(int year, int month, int day, String toParse){
+
+	void assertExpectedDate(int year, int month, int day, String toParse){
 		Calendar expected = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
 		expected.set(year, month, day, 0, 0, 0);
 		expected.set(Calendar.MILLISECOND, 0);
-		assertEquals(0, expected.compareTo(Iso8601Deserializer.toCalendar(toParse)));
-		assertEquals(expected.getTime(), Iso8601Deserializer.toDate(toParse));
+		expected.setMinimalDaysInFirstWeek(4);
+		expected.setFirstDayOfWeek(Calendar.MONDAY);
+
+		assertThat(Iso8601Deserializer.toCalendar(toParse)).isEqualTo(expected);
+		assertThat(Iso8601Deserializer.toDate(toParse)).isEqualTo(expected.getTime());
 	}
 }
